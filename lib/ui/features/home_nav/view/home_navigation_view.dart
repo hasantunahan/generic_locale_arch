2a95f5faef317e453edc17c08341fd63eb7457c5
HasanTunahan <name_tunahan@hotmail.com>
2021-12-11 16:13:19 +0300

    fix : bottombar

--- lib/ui/features/home_nav/view/home_navigation_view.dart
+++ lib/ui/features/home_nav/view/home_navigation_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:with_retro_firebase/core/base/view/baseview.dart';
+import 'package:with_retro_firebase/core/extension/context_extension.dart';
 import 'package:with_retro_firebase/ui/features/home_nav/viewmodel/home_navigation_viewmodel.dart';
 
 class HomeNavView extends StatelessWidget {
@@ -16,11 +17,13 @@ class HomeNavView extends StatelessWidget {
           model.init();
         },
         onPageBuilder: (BuildContext context, HomeNavViewModel viewModel) =>
-            renderBody(viewModel, theme));
+            renderBody(viewModel, theme, context));
   }
 
-  renderBody(HomeNavViewModel viewModel, ThemeData theme) {
+  renderBody(
+      HomeNavViewModel viewModel, ThemeData theme, BuildContext context) {
     return Scaffold(
+      extendBody: true,
       appBar: AppBar(
         toolbarHeight: 0,
         backgroundColor: theme.backgroundColor,
@@ -32,95 +35,121 @@ class HomeNavView extends StatelessWidget {
       floatingActionButtonLocation:
           FloatingActionButtonLocation.miniCenterDocked,
       floatingActionButton: FloatingActionButton(
+        backgroundColor: theme.colorScheme.primary,
         onPressed: () {},
-        child: Text(""),
+        child: const Icon(Icons.notifications),
       ),
       bottomNavigationBar: Observer(
         builder: (context) {
-          return renderBottomBar(viewModel, theme);
+          return renderBottomBar(viewModel, theme, context);
         },
       ),
     );
   }
 
-  renderBottomBar(HomeNavViewModel viewModel, ThemeData theme) {
-    return BottomNavigationBar(
-      currentIndex: viewModel.currentindex,
-      onTap: (value) {
-        viewModel.setCurrentIndex(value);
-      },
-      backgroundColor: theme.backgroundColor,
-      type: BottomNavigationBarType.fixed,
-      items: [
-        renderHome(theme),
-        renderProfile(theme),
-        renderChat(theme),
-        renderSettings(theme),
-      ],
-    );
-  }
-
-  renderSettings(ThemeData theme) {
-    return BottomNavigationBarItem(
-        activeIcon: Icon(
-          Icons.settings,
-          color: theme.colorScheme.primary,
-        ),
-        icon: Icon(Icons.settings_outlined, color: theme.colorScheme.onPrimary),
-        title: const SizedBox(
-          height: 0,
-        ));
-  }
-
-  renderChat(ThemeData theme) {
-    return BottomNavigationBarItem(
-      activeIcon: Icon(
-        Icons.chat,
-        color: theme.colorScheme.primary,
-      ),
-      icon: Icon(Icons.chat_outlined, color: theme.colorScheme.onPrimary),
-      title: const SizedBox(
-        height: 0,
+  renderBottomBar(
+      HomeNavViewModel viewModel, ThemeData theme, BuildContext context) {
+    return BottomAppBar(
+      shape: const CircularNotchedRectangle(),
+      elevation: 5,
+      notchMargin: 8,
+      color: theme.backgroundColor,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          renderHome(theme, viewModel, context),
+          renderProfile(theme, viewModel, context),
+          renderChat(theme, viewModel, context),
+          renderSettings(theme, viewModel, context)
+        ],
       ),
     );
   }
 
-  renderAdd(ThemeData theme) {
-    return BottomNavigationBarItem(
-        activeIcon: Icon(
-          Icons.add,
-          color: theme.colorScheme.primary,
-        ),
-        icon: Icon(Icons.add, color: theme.colorScheme.onPrimary),
-        title: const SizedBox(
-          height: 0,
-        ));
+  renderSettings(
+      ThemeData theme, HomeNavViewModel viewModel, BuildContext context) {
+    int index = 3;
+    return Observer(builder: (_) {
+      return GestureDetector(
+        onTap: () {
+          viewModel.setCurrentIndex(index);
+        },
+        child: Container(
+            height: 50,
+            width: context.width * .2,
+            child: index == viewModel.currentindex
+                ? Icon(Icons.settings, color: theme.colorScheme.primary)
+                : Icon(Icons.settings_outlined,
+                    color: theme.colorScheme.onPrimary)),
+      );
+    });
   }
 
-  renderProfile(ThemeData theme) {
-    return BottomNavigationBarItem(
-        activeIcon: Icon(
-          Icons.person,
-          color: theme.colorScheme.primary,
+  renderChat(
+      ThemeData theme, HomeNavViewModel viewModel, BuildContext context) {
+    int index = 2;
+    return Observer(builder: (_) {
+      return GestureDetector(
+        onTap: () {
+          viewModel.setCurrentIndex(index);
+        },
+        child: SizedBox(
+          height: 50,
+          width: context.width * .3,
+          child: index == viewModel.currentindex
+              ? Icon(
+                  Icons.chat,
+                  color: theme.colorScheme.primary,
+                )
+              : Icon(Icons.chat_outlined, color: theme.colorScheme.onPrimary),
         ),
-        icon: Icon(Icons.person_outline, color: theme.colorScheme.onPrimary),
-        title: const SizedBox(
-          height: 0,
-        ));
+      );
+    });
   }
 
-  renderHome(ThemeData theme) {
-    return BottomNavigationBarItem(
-        activeIcon: Icon(
-          Icons.home,
-          color: theme.colorScheme.primary,
+  renderProfile(
+      ThemeData theme, HomeNavViewModel viewModel, BuildContext context) {
+    int index = 1;
+    return Observer(builder: (_) {
+      return GestureDetector(
+        onTap: () {
+          viewModel.setCurrentIndex(index);
+        },
+        child: SizedBox(
+          height: 50,
+          width: context.width * .3,
+          child: index == viewModel.currentindex
+              ? Icon(
+                  Icons.person,
+                  color: theme.colorScheme.primary,
+                )
+              : Icon(Icons.person_outline, color: theme.colorScheme.onPrimary),
         ),
-        icon: Icon(
-          Icons.home_outlined,
-          color: theme.colorScheme.onPrimary,
-        ),
-        title: const SizedBox(
-          height: 0,
-        ));
+      );
+    });
+  }
+
+  renderHome(
+      ThemeData theme, HomeNavViewModel viewModel, BuildContext context) {
+    int index = 0;
+    return Observer(builder: (_) {
+      return GestureDetector(
+        onTap: () {
+          viewModel.setCurrentIndex(index);
+        },
+        child: SizedBox(
+            height: 50,
+            width: context.width * .2,
+            child: index == viewModel.currentindex
+                ? Icon(
+                    Icons.home,
+                    color: theme.colorScheme.primary,
+                  )
+                : Icon(
+                    Icons.home_outlined,
+                    color: theme.colorScheme.onPrimary,
+                  )),
+      );
+    });
   }
 }
