e8896ccc5570272fbb16b53bd2e27b089f32f5ee
hasantunahan <name_tunahan@hotmail.com>
2022-01-02 01:24:15 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -44,6 +44,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    //commit test
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       //i10n language provider
